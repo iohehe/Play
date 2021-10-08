27f438e0cd5e9f88e62104f946a56ec1ab55990b
iohehe <iohehe@yeah.net>
2021-10-08 19:38:47 +0800

    remove js files

--- AST/ASTNode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ASTNode = void 0;
-var ASTNode = /** @class */ (function () {
-    function ASTNode() {
-    }
-    return ASTNode;
-}());
-exports.ASTNode = ASTNode;
